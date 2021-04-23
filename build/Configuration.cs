87a24377df3df7659a00007c7234c19e4e97d3ac
Thomas Unger <474089+tunger@users.noreply.github.com>
2021-04-23 14:48:18 +0200

    update nuke

---
+++ build/Configuration.cs
@@ -0,0 +1,13 @@
+﻿using Nuke.Common.Tooling;
+
+[System.ComponentModel.TypeConverter(typeof(TypeConverter<Configuration>))]
+public class Configuration : Enumeration
+{
+    public static Configuration Debug = new Configuration { Value = nameof(Debug) };
+    public static Configuration Release = new Configuration { Value = nameof(Release) };
+
+    public static implicit operator string(Configuration configuration)
+    {
+        return configuration.Value;
+    }
+}
