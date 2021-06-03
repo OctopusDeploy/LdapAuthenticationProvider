f17811efa95285d85b1e75d8c589c49b9c4ca018
Mark Coafield <mark.coafield@octopus.com>
2021-06-04 09:32:18 +1000

    Formatting

--- source/Server/Configuration/ILdapConfigurationStore.cs
+++ source/Server/Configuration/ILdapConfigurationStore.cs
@@ -6,76 +6,62 @@ namespace Octopus.Server.Extensibility.Authentication.Ldap.Configuration
     public interface ILdapConfigurationStore : IExtensionConfigurationStore<LdapConfiguration>
     {
         string GetServer();
-
         void SetServer(string server);
 
         int GetPort();
-
         void SetPort(int port);
 
         void SetUseSsl(bool useSsl);
-
         bool GetUseSsl();
 
         void SetIgnoreSslErrors(bool ignoreSslErrors);
-
         bool GetIgnoreSslErrors();
 
         string GetConnectUsername();
-
         void SetConnectUsername(string username);
 
         SensitiveString GetConnectPassword();
-
         void SetConnectPassword(SensitiveString password);
 
         string GetBaseDn();
-
         void SetBaseDn(string baseDn);
 
         string GetDefaultDomain();
-
         void SetDefaultDomain(string defaultDomain);
 
         string GetUserNameAttribute();
-
         void SetUserNameAttribute(string userNameAttribute);
 
         string GetUserFilter();
-
         void SetUserFilter(string userFilter);
 
         string GetGroupFilter();
-
         void SetGroupFilter(string groupFilter);
 
         bool GetAllowAutoUserCreation();
         void SetAllowAutoUserCreation(bool allowAutoUserCreation);
 
         string GetUserDisplayNameAttribute();
-
         void SetUserDisplayNameAttribute(string userDisplayNameAttribute);
 
         string GetUserPrincipalNameAttribute();
-
         void SetUserPrincipalNameAttribute(string userPrincipalNameAttribute);
 
         string GetUserMembershipAttribute();
-
         void SetUserMembershipAttribute(string userMembershipAttribute);
 
         string GetUserEmailAttribute();
-
         void SetUserEmailAttribute(string userEmailAttribute);
 
         string GetGroupNameAttribute();
-
         void SetGroupNameAttribute(string groupNameAttribute);
         
         bool GetReferralFollowingEnabled();
         void SetReferralFollowingEnabled(bool enabled);
+
         int GetReferralHopLimit();
         void SetReferralHopLimit(int hopLimit);
+
         int GetConstraintTimeLimit();
         void SetConstraintTimeLimit(int timeLimit);
     }
