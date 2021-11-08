d7cf054ac6aa67c5de3403c5a7924325faeb349f
ryan.gribble <ryan.gribble@gmail.com>
2021-11-08 14:44:01 +1000

    add test data with special characters, add new integration test, add handling for more special chars

--- source/Ldap.Integration.Tests/scripts/ActiveDirectory/Remove-ActiveDirectoryIntegrationTestData.ps1
+++ source/Ldap.Integration.Tests/scripts/ActiveDirectory/Remove-ActiveDirectoryIntegrationTestData.ps1
@@ -7,3 +7,10 @@ Get-ADGroup "DeveloperGroup2" | Remove-ADGroup -Confirm:$false
 
 Get-ADUser "developer1" | Remove-ADUser -Confirm:$false
 Get-ADUser "developer2" | Remove-ADUser -Confirm:$false
+
+Get-ADGroup "SpecialGroup (with brackets)" | Remove-ADGroup -Confirm:$false
+Get-ADGroup "SpecialGroup# with a hash" | Remove-ADGroup -Confirm:$false
+Get-ADGroup "SpecialGroup_ with a comma" | Remove-ADGroup -Confirm:$false
+Get-ADGroup "SpecialGroup _ ( ) . & - _ _ _ ` ~ _ @ $ % ^ _ _ { } ! '" | Remove-ADGroup -Confirm:$false
+
+Get-ADUser "special#1" | Remove-ADUser -Confirm:$false
