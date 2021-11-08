709018a20d90acbdcfc81a68e6f7d05254cd2d5f
ryan.gribble <ryan.gribble@gmail.com>
2021-11-08 15:48:56 +1000

    add OpenLDAP test data and tests.  The comma is handled differently in openldap :(

--- source/Ldap.Integration.Tests/ICanSearchExternalGroupsTests.cs
+++ source/Ldap.Integration.Tests/ICanSearchExternalGroupsTests.cs
@@ -72,6 +72,25 @@ namespace Ldap.Integration.Tests
                 Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=DeveloperGroup1,ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
                 Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=DeveloperGroup2,ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
             }
+
+            [Fact]
+            internal void FindsGroupsFromOpenLDAPWithSpecialCharacters()
+            {
+                var partialName = "Special";
+                
+                ICanSearchExternalGroups fixture = FixtureHelper.CreateLdapExternalSecurityGroupLocator(ConfigurationHelper.GetOpenLdapConfiguration(), _testLogger);
+
+                var result = fixture.Search(partialName, new CancellationToken());
+
+                ExtensionResultHelper.AssertSuccesfulExtensionResult(result);
+                var searchResult = ((ResultFromExtension<ExternalSecurityGroupResult>)result).Value;
+
+                Assert.Equal(4, searchResult.Groups.Length);
+                Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=SpecialGroup (with brackets),ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
+                Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=SpecialGroup# with a hash,ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
+                Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=SpecialGroup\\2C with a comma,ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
+                Assert.Contains(searchResult.Groups, x => x.Id.Equals("cn=SpecialGroup * ( ) . & - _ [ ] ` ~ | @ $ % ^ ? : { } ! ',ou=Groups,dc=domain1,dc=local", StringComparison.InvariantCultureIgnoreCase));
+            }
         }
     }
 }
