4453ce805ad7dffa1e98e7e2d064a2652412c6df
zentron <robert.erez@gmail.com>
2022-04-11 14:47:19 +1000

    Back to pwsh

--- build/Build.cs
+++ build/Build.cs
@@ -132,7 +132,7 @@ class Build : NukeBuild
             var composeDirectory = SourceDirectory / "Ldap.Integration.Tests/scripts/OpenLdap/";
             Environment.SetEnvironmentVariable("OCTOPUS_LDAP_OPENLDAP_PORT", "3777");
 
-            using (var process = ProcessTasks.StartProcess("powershell", "./New-OpenLdapIntegrationTestEnvironment.ps1", composeDirectory))
+            using (var process = ProcessTasks.StartProcess("pwsh", "./New-OpenLdapIntegrationTestEnvironment.ps1", composeDirectory))
             {
                 process.AssertZeroExitCode();
             }
@@ -151,7 +151,7 @@ class Build : NukeBuild
             }
             finally
             {
-                using var process = ProcessTasks.StartProcess("powershell", "./Remove-OpenLdapIntegrationTestEnvironment.ps1", composeDirectory);
+                using var process = ProcessTasks.StartProcess("pwsh", "./Remove-OpenLdapIntegrationTestEnvironment.ps1", composeDirectory);
                 process.AssertZeroExitCode();
             }
 
